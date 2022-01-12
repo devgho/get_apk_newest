023eea2f4d7ac8b3ebd8816c5ca38c66ac38b6f1
devgho <devgho@outlook.com>
2022-01-12 19:48:20 +0800

    add except process

--- all_spider/pea_pods.py
+++ all_spider/pea_pods.py
@@ -12,8 +12,11 @@ def check(path="apks/dldl_9you/",serial="8130690"):
     _element = etree.HTML(resp.text)
     version =  str(_element.xpath("/html/body/div[2]/div[2]/div[2]/div[2]/div[1]/dl/dd[3]/text()")[0]).replace(u"\xa0",u"")
     if not os.path.exists(path+version+".apk"):
-        url = _element.xpath("/html/body/div[2]/div[2]/div[1]/div[2]/div[3]/a[1]/@href")[0] #没找到与页面中版本匹配的包的话就爬
-        download(url, version+".apk", path)
+        try:
+            url = _element.xpath("/html/body/div[2]/div[2]/div[1]/div[2]/div[3]/a[1]/@href")[0] #没找到与页面中版本匹配的包的话就爬            
+            download(url, version+".apk", path)
+        except IndexError:
+            check(path,serial)
 
 def main():
     check("apks/sgs_9you/",serial="6561883")
