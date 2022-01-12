663025d5b2241911c81d9b362756a1a0f87b852a
devgho <devgho@outlook.com>
2022-01-12 19:56:35 +0800

    fix

--- all_spider/pea_pods.py
+++ all_spider/pea_pods.py
@@ -10,13 +10,14 @@ from dl import download
 def check(path="apks/dldl_9you/",serial="8130690"):
     resp = requests.get("https://www.wandoujia.com/apps/"+serial)
     _element = etree.HTML(resp.text)
-    version =  str(_element.xpath("/html/body/div[2]/div[2]/div[2]/div[2]/div[1]/dl/dd[3]/text()")[0]).replace(u"\xa0",u"")
-    if not os.path.exists(path+version+".apk"):
-        try:
-            url = _element.xpath("/html/body/div[2]/div[2]/div[1]/div[2]/div[3]/a[1]/@href")[0] #没找到与页面中版本匹配的包的话就爬            
+    try:
+        version =  str(_element.xpath("/html/body/div[2]/div[2]/div[2]/div[2]/div[1]/dl/dd[3]/text()")[0]).replace(u"\xa0",u"")
+        if not os.path.exists(path+version+".apk"):
+            url = _element.xpath("/html/body/div[2]/div[2]/div[1]/div[2]/div[3]/a[1]/@href")[0] #没找到与页面中版本匹配的包的话就爬
             download(url, version+".apk", path)
-        except IndexError:
-            check(path,serial)
+    except IndexError:
+        check(path,serial)
+    
 
 def main():
     check("apks/sgs_9you/",serial="6561883")
